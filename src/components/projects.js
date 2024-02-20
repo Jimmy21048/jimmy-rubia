3aa31d1f550db1cfa1fd7a5025fcdb7e9c352ad9
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-02-20 15:54:34 +0300

    added project templates

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,6 +4,51 @@ export default function Projects() {
         <>
             <div className='projects-body'>
                 <Header />
+                <div className='projects hero1'>
+                    <h2>Library Management System</h2>
+                    <div className='projects-hero'>
+                        <a class='projects-link' href=''>
+                            <img src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F04%2F23%2F22%2F00%2Ftree-736885_640.jpg&tbnid=m57Fdarda7KehM&vet=12ahUKEwjJy42ox7mEAxUYT6QEHfP8B2EQMygMegQIARBj..i&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnatural%2F&docid=wTuoabapAcLfOM&w=640&h=398&q=image&ved=2ahUKEwjJy42ox7mEAxUYT6QEHfP8B2EQMygMegQIARBj' alt='' />
+                        </a>
+                        <div className='projects-hero-content'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                          Duis aute irure dolor
+                        </div>
+                    </div>    
+                </div>
+                <div className='projects hero1'>
+                    <h2>Church Website</h2>
+                    <div className='projects-hero'>
+                        <a class='projects-link' href=''>project1</a>
+                        <div className='projects-hero-content'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exerci
+                        </div>
+                    </div>  
+                </div>
+                <div className='projects hero1'>
+                    <h2>Title</h2>
+                    <div className='projects-hero'>
+                        <a class='projects-link' href=''>project1</a>
+                        <div className='projects-hero-content'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                         tempor incididun laborum.
+                        </div>
+                    </div>  
+                </div>
+                <div className='projects hero1'>
+                    <h2>Title</h2>
+                    <div className='projects-hero'>
+                        <a class='projects-link' href=''>project1</a>
+                        <div className='projects-hero-content'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                         tempor inborum.
+                        </div>
+                    </div>  
+                </div>
             </div>
         </>
     )
