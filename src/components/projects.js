cd19d1009b403e24575284b80f8921847911d30b
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-02-20 23:34:53 +0300

    added project library and church

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,25 +7,28 @@ export default function Projects() {
                 <div className='projects hero1'>
                     <h2>Library Management System</h2>
                     <div className='projects-hero'>
-                        <a class='projects-link' href=''>
-                            <img src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F04%2F23%2F22%2F00%2Ftree-736885_640.jpg&tbnid=m57Fdarda7KehM&vet=12ahUKEwjJy42ox7mEAxUYT6QEHfP8B2EQMygMegQIARBj..i&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnatural%2F&docid=wTuoabapAcLfOM&w=640&h=398&q=image&ved=2ahUKEwjJy42ox7mEAxUYT6QEHfP8B2EQMygMegQIARBj' alt='' />
+                        <a class='projects-link' href='https://online-library-o2us.onrender.com'>
+                            <img src='./images/lib3.png' alt='Book master' />
                         </a>
                         <div className='projects-hero-content'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                          Duis aute irure dolor
+                        "Welcome to BookMaster: Your Gateway to Library Management Excellence!
+                        Unlock the power of seamless book lending, member registration, and comprehensive
+                        book tracking with our cutting-edge library management website.
                         </div>
                     </div>    
                 </div>
                 <div className='projects hero1'>
                     <h2>Church Website</h2>
                     <div className='projects-hero'>
-                        <a class='projects-link' href=''>project1</a>
-                        <div className='projects-hero-content'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exerci
+                        <a class='projects-link' href='https://kag-maili4.netlify.app/'>
+                            <img src='./images/church1.png' alt='Book master' />
+                        </a>
+                        <div className='projects-hero-content'>  
+                            A church is a special place where hearts are lifted in worship,
+                            minds are nourished with timeless teachings, and souls find sanctuary
+                            in God's love. Find a place, a church that stands as a beacon of faith,
+                            offering a warm and welcoming environment for all who seek spiritual nourishment
+                            and community.
                         </div>
                     </div>  
                 </div>
