87ed475f11b11d59ead0ab541368d9b6eee50b00
Jimmy <rubiajimmy3@gmail.com>
2024-12-18 20:00:29 +0300

    tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,13 @@ import { Route, Routes } from "react-router-dom";
 
 export default function App() {
     return (
-        <div className='main-body'>
+        <>
             <Routes> 
                 <Route path="/" element = {<Home />}/>
                 <Route path="/about" element = {<About />} />
                 <Route path= "/tutorial/:id" element = {<Tutorial />} />
             </Routes>
-        </div>
+        </>
 
     )
 }
